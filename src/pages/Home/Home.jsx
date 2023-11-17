90c7e93f6979e92ffef68d24f7b900da2ddabddc
ladronbx <ladronbravovlc@gmail.com>
2023-11-17 20:40:55 +0100

    [feat] : added home image

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,12 @@
-import React, {useState, useEffect} from "react"
-import "./Home.css"
+import React from "react";
+import "./Home.css";
 
 export const Home = () => {
-
     return (
-        <div className="home-style">Soy el HOME</div>
-    )
-}
+
+        <div className="home-style">
+            <div className="title"> MalaTinta<br></br> Tattoo Studio</div>
+            <div className="logo"><img src="" alt="" /></div>
+        </div>
+    );
+};
