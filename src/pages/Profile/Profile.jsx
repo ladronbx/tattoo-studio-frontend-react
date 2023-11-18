8377c2d191faad36ea899965b025002499f752ef
ladronbx <ladronbravovlc@gmail.com>
2023-11-18 19:54:06 +0100

    [style]: Define color palette for the Profile view.

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,64 +1,61 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react";
 import "./Profile.css";
-import { getProfile } from "../../services/apiCalls"
-import { useNavigate } from 'react-router-dom'
-import { LinkButton } from '../../common/LinkButton/LinkButton'
+import { getProfile } from "../../services/apiCalls";
+import { useNavigate } from 'react-router-dom';
+import { LinkButton } from '../../common/LinkButton/LinkButton';
 import { useSelector } from "react-redux";
 import { selectToken } from "../userSlice";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+
 export const Profile = () => {
-
     const rdxToken = useSelector(selectToken);
-
     const [user, setUser] = useState({
         full_name: "",
         email: "",
         password: "",
         photo: "",
+        phone_number: "",
     });
-
     const navigate = useNavigate();
 
     useEffect(() => {
-
         if (rdxToken) {
             getProfile(rdxToken)
                 .then((response) => {
                     setUser(response.data.data);
                 })
-                .catch((error) => { console.log(error) });
-
+                .catch((error) => {
+                    console.log(error);
+                });
         } else {
             navigate("/login");
         }
-    }, []);
+    }, [rdxToken, navigate]);
 
     return (
-        <div class="card-user-container">
-            {
-                user
-                    ? (
-                        <div className="card-user">
-                            <img src={user.photo} alt="User" />
-                            <h2>{user.full_name}</h2>
-                            <h3>Email</h3>
-                            <h4>{user.email}</h4>
-                            <h3>Phone number</h3>
-                            <h4>{user.phone_number}</h4>
-                            <div>
-                                <LinkButton
-                                    classButton="button-update"
-                                    path={"/update"}
-                                    title={"Update my profile"}
-                                />
-                            </div>
+        <div className="container-main-profile">
+            {user ? (
+                <div className="container-profile mt-5">
+                    <img src={user.photo} alt="User"/>
+                    <div className="user-card-profile p-4">
+                        <div className="full-name-profile">{user.full_name}</div>
+                        <div className="email-name-profile">Email</div>
+                        <div className="email-profile">{user.email}</div>
+                        <div className="phone-name-profile">Phone number</div>
+                        <div className="phone-profile">{user.phone_number}</div>
+                        <div className="button-update">
+                            <LinkButton
+                                className="btn btn-primary"
+                                path={"/update"}
+                                title={"Update my profile"}
+                            />
                         </div>
-                    )
-                    : (
-                        <div>Loading ...</div>
-                    )
-            }
+                    </div>
+                </div>
+            ) : (
+                <div>Loading ...</div>
+            )}
         </div>
     );
-
 };
